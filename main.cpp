bc2a97a93675cb8580ec2b626b3c00d3530f09f4
polina <PAKulikova@stud.kantiana.ru>
2023-06-19 20:34:18 +0200

    brightness and contrast

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <stdio.h>
 #include "Detector_edge.hpp"
 #include "opencv2/imgproc/imgproc.hpp" //Новые функции обработки изображений, написанные на C++.
 //#include "opencv2/video/photo.hpp" //Алгоритмы обработки и восстановления фотографий.
@@ -11,9 +12,25 @@
 using namespace std;
 using namespace cv;
 namespace fs = std::filesystem; // Чтобы не писать `std::filesystem` каждый раз
+cv::Mat imgOriginal, img_bright;
+double alpha = 1.0; //< Simple contrast control
+int beta = 0; //< Simple brightness control
+void Bright_Threshold(){
+    for( int i = 0; i < imgOriginal.rows; i++ ) {
+        for( int j = 0; j < imgOriginal.cols; j++ ) {
+            for( int z = 0; z < imgOriginal.channels(); z++ ) {
+                img_bright.at<Vec3b>(j,i)[z] = saturate_cast<uchar>( alpha*imgOriginal.at<Vec3b>(j,i)[z] + beta );
+            }
+        }
+    }
+    imshow("Bright", img_bright);
+}
+
 int main()
 {
-    std::string directory_name = "/Users/polina/Desktop/С++/2_Project/cmake-build-debug";
+    cv::Mat imgOriginal;        // input image
+
+    std::string directory_name = "/Users/polina/Desktop/С++/2_Project/cmake-build-debug/";
     std::string extension = ".png";
 
     std::cout<<"Варианты файлов: ";
@@ -28,7 +45,8 @@ int main()
 
             // Проверяем, что имя заканчивается нужным расширением
             // В С++20 можно будет просто `bool match = name.ends_with(extension);`
-            bool match = !name.compare(name.size() - extension.size(), extension.size(), extension);
+            bool match = name.ends_with(extension);
+            //bool match = !name.compare(name.size() - extension.size(), extension.size(), extension);
             if (!match)
                 continue;
 
@@ -36,30 +54,28 @@ int main()
             std::cout << name << " ";
         }
         std::cout<<std::endl;
+        std::cout << "Введите название изображения : ";
+        std::string img_addr;
+        std::cin >> img_addr;
+
+        std::cout << "Поиск изображения " + img_addr << std::endl;
+        imgOriginal = cv::imread(directory_name + img_addr);            // open image
+
+        if (imgOriginal.empty())
+        {                                    // if unable to open image
+            std::cout << "error: изображение пустое.\n";        // show error message on command line
+            return(0);                                                // and exit program
+        }
     }
     catch (std::exception &e) {
         std::cout << "Error: " << e.what() << '\n';
     }
     //img = imread("animal.jpeg",-1);
 
-    cv::Mat imgOriginal;        // input image
     cv::Mat imgGrayscale;        // grayscale of input image
     cv::Mat imgBlurred;            // intermediate blured image
     cv::Mat imgCanny;            // Canny edge image
 
-
-    std::cout << "Введите название изображения : ";
-    std::string img_addr;
-    std::cin >> img_addr;
-
-    std::cout << "Поиск изображения " + img_addr << std::endl;
-    imgOriginal = cv::imread(img_addr);            // open image
-
-    if (imgOriginal.empty())
-    {                                    // if unable to open image
-        std::cout << "error: изображение не найдено.\n";        // show error message on command line
-        return(0);                                                // and exit program
-    }
     cvtColor(imgOriginal, gray, COLOR_BGR2GRAY);//преобразование изображения в оттенки серого
     cv::namedWindow("Original", WINDOW_AUTOSIZE);
     //cv::namedWindow("Gray", WINDOW_AUTOSIZE);
@@ -76,9 +92,56 @@ int main()
     imshow("Edge Detection",edge);
 
     ///Negative
-    cv::Mat img_neg, img_rgb;
-    img_neg = cv::Scalar(255,255,255)-imgOriginal; ///Вычисляем негатив
+    cv::Mat img_neg;
+    img_neg = cv::Scalar(255,255,255) - imgOriginal; ///Вычисляем негатив
     cv::imshow("Negative", img_neg);
+
+    ///Яркость
+    cv::Mat img_bright;
+    cv::namedWindow("Bright", WINDOW_AUTOSIZE);
+    //img_bright = imgOriginal + 40;
+
+    int min_bright = 0;
+    int max_bright = 100;
+    /*void Bright_Threshold(){
+        for( int i = 0; i < imgOriginal.rows; i++ ) {
+            for( int j = 0; j < imgOriginal.cols; j++ ) {
+                for( int z = 0; z < imgOriginal.channels(); z++ ) {
+                    img_bright.at<Vec3b>(y,x)[c] = saturate_cast<uchar>( alpha*imgOriginal.at<Vec3b>(y,x)[c] + beta );
+                }
+            }
+        }
+        imshow("Bright", img_bright)
+    }*/
+    //Bright_Threshold(0, 0);
+
+    cv::createTrackbar("Brightness", "Bright", &min_bright, max_bright,
+                       reinterpret_cast<TrackbarCallback>(Bright_Threshold));
+    cv::imshow("Bright", img_bright);
+
+    ///RGB в отдельныъ окнах
+    cv::Mat sum_rgb;  // variable image of datatype Matrix
+   // cv::imshow("Display Image", imgOriginal);
+
+    // three channel to store b, g, r
+    cv::Mat rgbchannel[3];
+
+    // split image
+    cv::split(imgOriginal, rgbchannel);
+
+    // plot individual component
+    //cv::namedWindow("Blue",WINDOW_AUTOSIZE);
+    //cv::imshow("Red", rgbchannel[0]);
+
+    //cv::namedWindow("Green",WINDOW_AUTOSIZE);
+    //cv::imshow("Green", rgbchannel[1]);
+
+    //cv::namedWindow("Red",WINDOW_AUTOSIZE);
+    //cv::imshow("Blue", rgbchannel[2]);
+
+    // merge : (input, num_of_channel, output)
+    cv::merge(rgbchannel, 3, sum_rgb);
+    //cv::imshow("Merged", sum_rgb);
 
     waitKey(0);
 return 0;
